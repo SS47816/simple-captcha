655ea663cba9a609f641d7df18c97167019d62f6
SS47816 <ss1057532022@gmail.com>
2025-03-25 16:26:11 +0800

    feat: extract templates

---
+++ simple-captcha/pipeline/captcha.py
@@ -0,0 +1,12 @@
+class Captcha(object):
+    def __init__(self):
+        pass
+
+    def __call__(self, im_path, save_path):
+        """
+        Algo for inference
+        args:
+            im_path: .jpg image path to load and to infer
+            save_path: output file path to save the one-line outcome
+        """
+        pass
